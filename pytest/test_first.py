e70c7c68faa67f7beb7b8a339cbc890a57e6430d
demetira komuhangi <demetirakomuhangi2023@gmail.com>
2023-10-14 20:01:44 +0300

    3rdcycle

--- pytest/test_first.py
+++ pytest/test_first.py
@@ -6,15 +6,20 @@
 #     result =multiply(1, 1)
 #     assert  result  == 1
 
-#     assert multiply(2, 2) == 4
+# assert multiply(2, 2) == 4
+
 
 def multiply(a, b):
-        # if a ==3 and  b==3:
-        #     return 10
-        return a + b
-    
-def test_multiply():
-        result=multiply(1,1)
-        assert result==1
-        assert multiply(3,3) == 9
+    # if a ==3 and  b==3:
+    #     return 10
+    return a + b
 
+
+def test_multiply():
+    result = multiply(1, 1)
+    assert result == 1
+    result2 = multiply(2, 2)
+    assert result2 == 4
+    result3 = multiply(3, 3)
+
+    assert multiply(3, 3) == 9
