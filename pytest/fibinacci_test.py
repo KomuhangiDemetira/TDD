cd2a3cfb450501b329cd356cb016e0f8de4c2945
demetira komuhangi <demetirakomuhangi2023@gmail.com>
2023-10-17 14:37:39 +0300

    fabonacci_sequence work

---
+++ pytest/fibinacci_test.py
@@ -0,0 +1,11 @@
+def generate_fibonacci(n):
+    fibonacci_sequence = [0, 1]
+
+    while len(fibonacci_sequence) < n:
+        next_term = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_term)
+
+    return fibonacci_sequence
+n_terms = 10
+result = generate_fibonacci(n_terms)
+print(result)
